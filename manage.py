c0a84db33e4fa6dd2620bd4ec051540643cbbfd9
David Lopes <davidribeirolopes@gmail.com>
2018-09-13 14:29:17 -0300

    Adds AppD

--- manage.py
+++ manage.py
@@ -5,6 +5,18 @@ import sys
 from appdynamics.agent import api as appd
 
 if __name__ == '__main__':
+
+    appd.init(
+        {'APPD_APP_NAME': 'Python',
+         'APPD_TIER_NAME': 'django',
+         'APPD_NODE_NAME': 'dlopes-mac',
+         'APPD_CONTROLLER_HOST': 'controller',
+         'APPD_CONTROLLER_PORT': '8090',
+         'APPD_ACCOUNT_ACCESS_KEY': 'access_key',
+         'APPD_LOGGING_LEVEL': 'debug'
+         }
+    )
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appdasync.settings')
     try:
         from django.core.management import execute_from_command_line
--- polls/views.py
+++ polls/views.py
@@ -3,20 +3,31 @@ import time
 from django.http import HttpResponse
 from concurrent.futures import ThreadPoolExecutor
 
+from appdynamics.agent import api as appd
+
 
 class Slow1():
+    def __init__(self):
+        self.name = 'Slow1'
+
     def run_check(self):
         time.sleep(1)
         return 1
 
 
 class Slow2():
+    def __init__(self):
+        self.name = 'Slow2'
+
     def run_check(self):
         time.sleep(2)
         return 2
 
 
 class Slow3():
+    def __init__(self):
+        self.name = 'Slow3'
+
     def run_check(self):
         time.sleep(3)
         return 3
@@ -26,12 +37,25 @@ plugins = [Slow1(), Slow2(), Slow3()]
 
 
 def _run(plugin):
-    return plugin.run_check()
+    start = time.time()
+    ret = plugin.run_check()
+    return (ret, plugin.name, time.time() - start)
 
 
 def index(request):
+
+    appd_bt = appd.get_active_bt_handle(request)
+
     with ThreadPoolExecutor(max_workers=5) as executor:
         for ers in executor.map(_run, plugins):
+
+            # If we have a Business Transaction, lets add snapshot data
+            # Another way to do this is to pass the request object directly
+            # In this case we would create the BTs directly on the objects
+            # (plugin) code
+            if(appd_bt):
+                print('Adding snapshot data')
+                appd.add_snapshot_data(appd_bt, ers[1], ers)
             print(ers)
 
     return HttpResponse("Hello, world. You're at the polls index.")
